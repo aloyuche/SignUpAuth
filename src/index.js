3433a607b404e4117643d79e597e81dcb1869c2e
aloyuche <aloynwa@gmail.com>
2022-10-26 11:59:37 +0100

    sace

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
+import { AuthProvide } from "./components/context/AuthProvide";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvide>
+      <App />
+    </AuthProvide>
   </React.StrictMode>
 );
