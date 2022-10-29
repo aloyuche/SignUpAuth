8393332820ed1709806dc1e3f78a661e627438ee
aloyuche <aloynwa@gmail.com>
2022-10-29 08:19:40 +0100

    Signup

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -105,7 +105,7 @@ const Register = () => {
           </p>
         </section>
       ) : (
-        <section>
+        <FormContainer>
           <p
             ref={errRef}
             className={errMsg ? "errmsg" : "offscreen"}
@@ -247,10 +247,65 @@ const Register = () => {
               <a href="/login">Sign In</a>
             </span>
           </p>
-        </section>
+        </FormContainer>
       )}
     </>
   );
 };
 
+const FormContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    gap = 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: colunm;
+    background:#131324;
+    .brand{
+        display:flex;
+        justify-content: center;
+        align-items:center;
+        gap: 1rem;
+        h1{
+        font-size:2.8rem;
+        color:white;
+        
+        }
+        img{
+            width: 20%;
+        }
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        gap:1.2rem;
+        background:#00000076;
+        border-radius:2rem;
+        padding: 2rem 4rem;
+
+        input{
+            border:0.1px solid #4e0eff;
+            padding: 1rem;
+            background: transparent;
+            border-radius: 0.4rem;
+            color:white;
+            width:100%;
+            font-size:1rem;
+            
+        }
+
+    }
+    button{
+        padding: 1rem 2rem;
+        background: #997af0;
+        color:white;
+        width:100%;
+        font-weight:bold;
+        border-radius: 1rem;
+    }
+    p{
+        color: orange;
+        font-size:1.2rem
+    }`;
 export default Register;
